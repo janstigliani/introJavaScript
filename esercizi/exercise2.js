fa675828178213a8901dc540f06b510bbc50c744
JANBEAST\Jan <janstigli@gmail.com>
2025-03-05 23:28:40 +0100

    code wars 11

--- esercizi/exercise2.js
+++ esercizi/exercise2.js
@@ -275,3 +275,18 @@ function makeValley(arrayOfInt) {
 
 const roba = makeValley([]);
 console.log(roba);
+
+//ESERCIZIO 11 CODE-WARS (Find twins)
+
+function elimination(arr) {
+    for (i = 0; i < arr.length; i++) {
+        const int1 = arr[i]
+        for (j = i+1; j < arr.length; j++) {
+            const int2 = arr[j]
+            if (int1 === int2) {
+                return int1;
+            }
+        }
+    }
+    return null;
+}
